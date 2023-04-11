df7b950cb8c1fbdc67c76e3b9eea2a050a80ffa9
Renan <renan.fachin@hotmail.com>
2023-04-11 17:48:53 -0300

    Streams - corpo da requisição em JSON

--- src/server.js
+++ src/server.js
@@ -11,8 +11,22 @@ const users = []
 
 // Criando servidor http
 // createServer recebe 2 parâmetros: request e response
-const server = http.createServer((request, response) => {
+const server = http.createServer(async (request, response) => {
   const { method, url } = request
+
+  const buffers = []
+
+  // percorrer cada pedaço da stream de requisição e adicionar ao array de buffers
+  for await (const chunk of request) {
+    buffers.push(chunk)
+  }
+
+  // o await não permite que nada execute enquanto o carregamento da stream não finalizar
+  try {
+    request.body = JSON.parse(Buffer.concat(buffers).toString())
+  } catch {
+    request.body = null
+  }
 
 
   // Exemplo de uma mesma url com diferentes métodos e diferentes retornos
@@ -25,10 +39,14 @@ const server = http.createServer((request, response) => {
   }
 
   if (method === 'POST' && url === '/users') {
+    // Buscando da request o name e o email do novo usuário
+    const { name, email } = request.body
+
+
     users.push({
       id: 1,
-      name: 'John Doe',
-      email: 'johndoe@example.com'
+      name: name,
+      email: email
     })
 
     return response.writeHead(201).end()
