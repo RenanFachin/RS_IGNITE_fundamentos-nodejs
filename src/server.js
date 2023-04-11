a0a6ecda38a655ab4fb991b9604da997ed7fc141
Renan <renan.fachin@hotmail.com>
2023-04-11 15:03:56 -0300

    Persistindo dados em memória (stateful) e defininindo headers

--- src/server.js
+++ src/server.js
@@ -2,6 +2,13 @@
 // Para importação de módulos internos, a documentação determina que devemos utilizar 'node:'
 import http from 'node:http'
 
+// Aplicação em stateful -> os dados sao guardados em memória e são apagados assim que a aplicação for derrubada
+
+// Cabeçalhos (Requisição/Resposnse) => metadados
+
+const users = []
+
+
 // Criando servidor http
 // createServer recebe 2 parâmetros: request e response
 const server = http.createServer((request, response) => {
@@ -9,11 +16,21 @@ const server = http.createServer((request, response) => {
 
 
   // Exemplo de uma mesma url com diferentes métodos e diferentes retornos
-  if(method === 'GET' && url === '/users'){
-    return response.end('Listagem de usuários')
+  if (method === 'GET' && url === '/users') {
+
+    // Transformando o retorno em JSON - Javascript Object Notation
+    return response
+      .setHeader('Content-type', 'application/json')
+      .end(JSON.stringify(users))
   }
 
-  if(method === 'POST' && url === '/users'){
+  if (method === 'POST' && url === '/users') {
+    users.push({
+      id: 1,
+      name: 'John Doe',
+      email: 'johndoe@example.com'
+    })
+
     return response.end('Criação de usuário')
   }
 
