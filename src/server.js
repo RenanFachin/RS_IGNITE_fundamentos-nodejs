a1f9e7ccf1c340d011ac34470ad0525eb182b28d
Renan <renan.fachin@hotmail.com>
2023-04-11 14:31:18 -0300

    cria servidor node utilizando o módulo http

---
+++ src/server.js
@@ -0,0 +1,11 @@
+// importando o módulo de http
+// Para importação de módulos internos, a documentação determina que devemos utilizar 'node:'
+import http from 'node:http'
+
+// Criando servidor http
+// createServer recebe 2 parâmetros: request e response
+const server = http.createServer((request, response) => {
+  return response.end('Hello world')
+})
+
+server.listen(3333) // localhost:3333
