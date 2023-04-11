1f42191bddf5e53112f543bd0d5b5026ee07f835
Renan <renan.fachin@hotmail.com>
2023-04-11 14:49:25 -0300

    Métodos HTTP

--- src/server.js
+++ src/server.js
@@ -5,6 +5,18 @@ import http from 'node:http'
 // Criando servidor http
 // createServer recebe 2 parâmetros: request e response
 const server = http.createServer((request, response) => {
+  const { method, url } = request
+
+
+  // Exemplo de uma mesma url com diferentes métodos e diferentes retornos
+  if(method === 'GET' && url === '/users'){
+    return response.end('Listagem de usuários')
+  }
+
+  if(method === 'POST' && url === '/users'){
+    return response.end('Criação de usuário')
+  }
+
   return response.end('Hello world!')
 })
 
